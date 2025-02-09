9500397305814bee62f6c2c0f621bcc9a9713ef2
andrepassos.compositor <andrepassos.compositor@gmail.com>
2025-02-09 17:33:52 -0300

    fix wide

--- src/features/Hero/index.jsx
+++ src/features/Hero/index.jsx
@@ -34,8 +34,8 @@ function Hero() {
 
   return (
     <section className="App-hero bg-white text-black flex bellota-text-regular">
-      <div className="hero-piano flex w-full h-1/3 relative h-screen align-left border-4">
-        <div className="side-key hidden xl:block bg-dark w-1/6 p-6 sticky top-0 h-screen">
+      <div className="hero-piano flex w-full h-1/3 relative align-left border-4">
+        <div className="side-key hidden xl:block bg-dark w-1/6 p-6 sticky top-0 ">
           <Menu styleClass="flex flex-col space-y-4" />
         </div>
         <div className="keyboard flex w-full xl:w-5/6">
