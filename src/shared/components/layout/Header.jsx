8ddc546dd43f308fbece34ac623516db3c6fb3b4
andrepassos.compositor <andrepassos.compositor@gmail.com>
2025-02-24 09:24:35 -0300

    fix contact translations

--- src/shared/components/layout/Header.jsx
+++ src/shared/components/layout/Header.jsx
@@ -5,9 +5,9 @@ import Menu from '../common/Menu';
 function Header() {
 
     return (
-        <header className='xl:hidden'>
+        <header className='xl:hidden '>
             <div className="header-container mx-auto flex justify-center md:block bg-dark text-white
-             py-4 fixed z-10 w-full special-elite-regular">
+             py-4 xsm:py-2 fixed z-10 w-full special-elite-regular">
                 <Menu />
             </div>
         </header>
