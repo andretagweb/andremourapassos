2b2fa858b90ead229206824df81b3a0407ef5013
andrepassos.compositor <andrepassos.compositor@gmail.com>
2025-04-25 14:11:58 -0300

    mobile text size fix

--- src/features/Hero/components/Keys/index.jsx
+++ src/features/Hero/components/Keys/index.jsx
@@ -58,7 +58,7 @@ function Keys({ playPlaylist }) {
           <h1 className="hero-title text-lg sm:text-3xl md:text-3xl lg:text-4xl drop-shadow-sm font-bold lg:mb-6 text-white limelight-regular">
             {t("main_title")}
           </h1>
-          <h2 className="text-sm md:text-md lg:text-xl drop-shadow-sm font-bold lg:mb-6 text-white">
+          <h2 className="subtitle text-sm md:text-md lg:text-xl drop-shadow-sm font-bold lg:mb-6 text-white">
             {t("subtitle_1")}, <span className="text-gray-medium font-light">{t("subtitle_2")},</span> {t("subtitle_3")}
           </h2>
         </div>
