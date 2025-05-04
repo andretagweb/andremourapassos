c137f874b71b3e3d867331877169f0f02767d1bc
andrepassos.compositor <andrepassos.compositor@gmail.com>
2025-05-03 21:26:22 -0300

    use downloaded fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import './shared/styles/index.css' ;
+import './shared/styles/fonts.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
