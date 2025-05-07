746f435dd6ae612c6d30cb03cb950300ace684fd
andrepassos.compositor <andrepassos.compositor@gmail.com>
2025-05-07 10:32:42 -0300

    head dinamico

--- src/shared/components/common/SEO.js
+++ src/shared/components/common/SEO.js
@@ -3,13 +3,26 @@ import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
 
 const SEO = () => {
-  const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation("seo");
 
-  const siteTitle = t("common:siteTitle", "André Moura Passos | Compositor & Desenvolvedor");
-  const siteDescription = t("common:siteDescription", "Explore as músicas instrumentais e projetos de desenvolvimento de André Moura Passos.");
-  const siteImage = "https://andremourapassos.com/logo192.png";
-  const siteUrl = "https://andremourapassos.com";
-  const currentLang = i18n.language;
+  const currentLang = i18n.language.split("-")[0]; // pt, en, es
+  const siteUrl = "https://www.andremourapassos.com";
+  const siteImage = `${siteUrl}/logo192.png`;
+
+  const localizedUrls = {
+    pt: `${siteUrl}/pt`,
+    en: `${siteUrl}/en`,
+    es: `${siteUrl}/es`
+  };
+
+  const canonicalPath = window.location.pathname.replace(/\/$/, "") || "/";
+  const canonicalUrl = `${siteUrl}${canonicalPath}`;
+
+  const localeMap = {
+    pt: "pt_BR",
+    en: "en_US",
+    es: "es_ES"
+  };
 
   const schemaData = {
     "@context": "https://schema.org",
@@ -23,10 +36,10 @@ const SEO = () => {
       "https://www.facebook.com/people/Andre-Moura-Passos/100090936608265/",
       "https://www.instagram.com/samebloodscocktail/"
     ],
-    "jobTitle": t("common:jobTitle", "Compositor e Desenvolvedor"),
+    "jobTitle": t("job_title", "Compositor e Desenvolvedor"),
     "worksFor": {
       "@type": "Organization",
-      "name": t("common:organizationName", "Projeto de Música Independente")
+      "name": t("organization_name", "Projeto de Música Independente")
     }
   };
 
@@ -34,29 +47,33 @@ const SEO = () => {
     <>
       <Helmet>
         <html lang={currentLang} />
-        <title>{siteTitle}</title>
-        <meta name="description" content={siteDescription} />
-        <meta name="keywords" content="André Moura Passos, compositor, desenvolvedor, música instrumental, piano, criação musical, site pessoal" />
+        <title>{t("title")}</title>
+        <meta name="description" content={t("description")} />
+        <meta name="keywords" content={t("keywords")} />
         <meta name="author" content="André Moura Passos" />
         <meta name="robots" content="index, follow" />
 
         {/* Open Graph */}
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:description" content={siteDescription} />
+        <meta property="og:title" content={t("og_title")} />
+        <meta property="og:description" content={t("og_description")} />
         <meta property="og:image" content={siteImage} />
         <meta property="og:url" content={siteUrl} />
         <meta property="og:type" content="website" />
-        <meta property="og:locale" content={
-          currentLang === 'pt' ? 'pt_BR' :
-          currentLang === 'es' ? 'es_ES' :
-          'en_US'
-        } />
+        <meta property="og:locale" content={localeMap[currentLang] || "pt_BR"} />
+        <meta property="og:site_name" content="André Moura Passos" />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={siteTitle} />
-        <meta name="twitter:description" content={siteDescription} />
+        <meta name="twitter:title" content={t("twitter_title")} />
+        <meta name="twitter:description" content={t("twitter_description")} />
         <meta name="twitter:image" content={siteImage} />
+
+        {/* Canonical + hreflang */}
+        <link rel="canonical" href={canonicalUrl} />
+        <link rel="alternate" hreflang="pt" href={localizedUrls.pt} />
+        <link rel="alternate" hreflang="en" href={localizedUrls.en} />
+        <link rel="alternate" hreflang="es" href={localizedUrls.es} />
+        <link rel="alternate" hreflang="x-default" href={siteUrl} />
       </Helmet>
 
       {/* Structured Data */}
--- src/shared/locales/i18n.js
+++ src/shared/locales/i18n.js
@@ -2,32 +2,59 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+// EN
 import enCommon from "./en/common.json";
-import enkeys from "./en/keys.json";
+import enKeys from "./en/keys.json";
 import enMenu from "./en/menu.json";
 import enPlayer from "./en/player.json";
 import enFooter from "./en/footer.json";
+import enSeo from "./en/seo.json";
 
+// PT
 import ptCommon from "./pt/common.json";
-import ptkeys from "./pt/keys.json";
+import ptKeys from "./pt/keys.json";
 import ptMenu from "./pt/menu.json";
 import ptPlayer from "./pt/player.json";
 import ptFooter from "./pt/footer.json";
+import ptSeo from "./pt/seo.json";
 
+// ES
 import esCommon from "./es/common.json";
-import eskeys from "./es/keys.json";
+import esKeys from "./es/keys.json";
 import esMenu from "./es/menu.json";
 import esPlayer from "./es/player.json";
 import esFooter from "./es/footer.json";
+import esSeo from "./es/seo.json";
 
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
     resources: {
-      en: { common: enCommon, keys: enkeys, menu: enMenu, player: enPlayer, footer: enFooter },
-      pt: { common: ptCommon, keys: ptkeys, menu: ptMenu, player: ptPlayer, footer: ptFooter },
-      es: { common: esCommon, keys: eskeys, menu: esMenu, player: esPlayer, footer: esFooter }
+      en: {
+        common: enCommon,
+        keys: enKeys,
+        menu: enMenu,
+        player: enPlayer,
+        footer: enFooter,
+        seo: enSeo
+      },
+      pt: {
+        common: ptCommon,
+        keys: ptKeys,
+        menu: ptMenu,
+        player: ptPlayer,
+        footer: ptFooter,
+        seo: ptSeo
+      },
+      es: {
+        common: esCommon,
+        keys: esKeys,
+        menu: esMenu,
+        player: esPlayer,
+        footer: esFooter,
+        seo: esSeo
+      }
     },
     fallbackLng: "en",
     debug: false,
